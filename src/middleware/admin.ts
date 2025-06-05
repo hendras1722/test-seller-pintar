ee20367ae92f6583dcb167b7f24881a8d057657e
Muh Syahendra Anindyantoro <57746184+hendras1722@users.noreply.github.com>
2025-06-05 14:33:53 +0700

    fix: comment out unused token handling and getMe function in admin middleware

--- src/middleware/admin.ts
+++ src/middleware/admin.ts
@@ -5,28 +5,28 @@ import { getMe } from '@/api/auth'
 export async function adminMiddleware(request: NextRequest) {
   const response = NextResponse.next()
 
-  const token = request.cookies.get('token')
-  const { pathname } = new URL(request.url)
+  // const token = request.cookies.get('token')
+  // const { pathname } = new URL(request.url)
   
-  if (token) {
-    const getMeResponse = await getMe()
+  // if (token) {
+  //   const getMeResponse = await getMe()
 
-    response.cookies.set('me', JSON.stringify(getMeResponse.data), {
-      path: '/admin', 
-      maxAge: 60 * 60 * 24 * 7,
-      sameSite: 'lax',
-      secure: true
-    })
+  //   response.cookies.set('me', JSON.stringify(getMeResponse.data), {
+  //     path: '/admin', 
+  //     maxAge: 60 * 60 * 24 * 7,
+  //     sameSite: 'lax',
+  //     secure: true
+  //   })
     
-    if (pathname.startsWith('/login')){
-      return NextResponse.redirect(new URL('/admin/category', request.url))
-    }
-  } else {
-    // response.cookies.delete('me')
-    if (pathname.startsWith('/admin')) {
-      return NextResponse.redirect(new URL('/login', request.url))
-    }
-  }
+  //   if (pathname.startsWith('/login')){
+  //     return NextResponse.redirect(new URL('/admin/category', request.url))
+  //   }
+  // } else {
+  //   // response.cookies.delete('me')
+  //   if (pathname.startsWith('/admin')) {
+  //     return NextResponse.redirect(new URL('/login', request.url))
+  //   }
+  // }
 
   return response
 }
